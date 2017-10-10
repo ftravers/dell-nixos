e532ffb08f09955fc371e5113bd5736214bdb858
Fenton Travers <fenton.travers@gmail.com>
2017-10-10 11:22:04 -0700

    x working now too

--- configuration.nix
+++ configuration.nix
@@ -80,14 +80,14 @@
 
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
 
 
   # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
 
   system.stateVersion = "17.09";
