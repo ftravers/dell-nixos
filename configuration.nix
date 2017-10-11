8b69111d9724f57c2f64a3351f4c70f606d11dbf
Fenton Travers <fenton.travers@gmail.com>
2017-10-10 20:57:59 -0700

    can ssh into container from remote machine

--- configuration.nix
+++ configuration.nix
@@ -16,37 +16,32 @@
   networking = {
     # networkmanager.enable = true;
 
-
     interfaces.wlp1s0.ip4 = [ { address = "192.168.0.150"; prefixLength = 24; } ];
     defaultGateway = "192.168.0.1";
     nameservers = [ "8.8.8.8" ];
 
-
+    nat = {
+      enable = true;
+      internalInterfaces = ["ve-+"];
+      externalInterface = "wlp1s0";
+      forwardPorts = [
+        { sourcePort = 2222; destination = "192.168.0.151:22"; }
+      ];
+    };
+  
     wireless = {
       enable = true;
       interfaces = ["wlp1s0"];
       userControlled.enable = true;
       userControlled.group = "wheel";
       networks = {
-	"1529-upstairs-2.4" = {
-	  psk = "abc7654321";
-	};
+      	"1529-upstairs-2.4" = {
+      	  psk = "abc7654321";
+        };
       };
     };
 
-    hostName = "delldesk"; # Define your hostname.
-
-    # interfaces.wlp1s0.ip4 = [ { address = "192.168.0.201"; prefixLength = 24; } ];
-    # defaultGateway = "192.168.0.1";
-    # nameservers = [ "8.8.8.8" ];
-    # nat = {
-    #   enable = true;
-    #   internalInterfaces = ["ve-+"];
-    #   externalInterface = "wlp1s0";
-    #   forwardPorts = [
-    #     { sourcePort = 2222; destination = "192.168.0.202:22"; }
-    #   ];
-    # };
+    hostName = "delldesk"; 
   };
 
   users.extraUsers.fenton = {
@@ -77,8 +72,9 @@
 
 
   # Packages ############################################
+  # -------- #
   environment.systemPackages = with pkgs; [
-    wget vim dhcpcd emacs firefox chromium git mkpasswd
+    wget vim dhcpcd emacs firefox chromium git mkpasswd zsh rxvt_unicode
     autoconf gnumake tree ncurses rxvt_unicode wpa_supplicant
   ];
 
@@ -105,5 +101,49 @@
   services.xserver.desktopManager.plasma5.enable = true;
 
 
+
+ # System Default Shell ##########################
+ # -------------------- #
+ users.defaultUserShell = "/var/run/current-system/sw/bin/zsh";
+ environment.sessionVariables = {
+   TERMINFO_DIRS = "/run/current-system/sw/share/terminfo";
+ };
+
+
   system.stateVersion = "17.09";
+
+
+  # Containers
+      containers.fud = {
+      autoStart = true;
+      privateNetwork = true;
+      hostAddress = "192.168.0.150";
+      localAddress = "192.168.0.151";
+      config = { config, pkgs, ...}: {
+
+        # Set your time zone.
+        time.timeZone = "America/Vancouver";
+
+        networking = {
+          firewall.enable = false;
+          hostName = "fud"; 
+        };
+
+        services.openssh.enable = true;
+
+        # List packages installed in system profile
+        environment.systemPackages = with pkgs; [
+          wget vim emacs git zsh autoconf gnumake tree
+          ncurses rxvt_unicode openjdk leiningen unzip
+        ];
+
+        users.extraUsers.fenton = {
+          isNormalUser = true;
+          uid = 1000;
+          hashedPassword = "$6$/pK0lFCWLEBBWMJ$S9vMo8Qd2YL1jRuw4S8O7xdB/S4bBaPUVgWQOCkfUK19fJAD2cVGwRKG3017YnbjzFbMkVG5Xe3RuNhWY6zZY/";
+          extraGroups = [ "wheel" ];
+          openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAG5wvPKRrqqiOBKZ5dUqF5oK7vd1zNuVsrrAAQCcEkVC2SBXVy5yiCiO7xPz7Wk6oSl+5nvkitYQ4HVuNO+mroUcmbge/e344sfyOytrV2BqFTuijlc+BkBTMk55piHKBgl50l4gIdtTdKk1b0iiTxc5gdhlUr4LUF+mPc5NnuKgMEJLApoFeNKrzbR+Z5ZsLypeFNxzkaAw8mjqRoDoi1lab7tDN/KrVKZ46AYXm9Tix64MdxXI6T+p6Z+2rAQQ0ieexVtVUJBiifaKrvqgr57v8WPFk8VIYb9MbtlxtHdHz/regzZA4L6K+46QpSFeBX29esx1/tuihv/hU8ndf fenton@ss9" ];
+        };
+      };
+    }; 
 }
